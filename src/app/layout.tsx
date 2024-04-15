ae70c98701e4d5bb9137d8c900d88e008d6e8f5e
Harrison <hmichas@gmail.com>
2024-04-15 16:19:12 -0400

    bootstrap setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
