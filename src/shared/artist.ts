bb7ac66dd8d3f1ff3266c40a6b6567718e01f0db
Harrison <hmichas@gmail.com>
2024-04-21 20:34:41 -0400

    added non-relational fields to Artist Entity

--- src/shared/artist.ts
+++ src/shared/artist.ts
@@ -13,4 +13,23 @@ export class Artist {
     @Fields.updatedAt()                       // The date and time this artist entity was last updated
     updatedAt = new Date()
 
+    @Fields.string()                          // The last name of the artist
+    lastName = '';
+
+    @Fields.string()                          // The first name of the artist
+    firstName = '';
+
+    @Fields.dateOnly({ allowNull: true })     // The birth date of the artist
+    birthDate?: Date;
+
+    @Fields.dateOnly({ allowNull: true })     // The death date of the artist
+    deathDate?: Date;
+
+    @Fields.string({ allowNull: true })       // The birth location of the artist
+    birthLocation?: string;
+
+    @Fields.string({ allowNull: true })       // The death location of the artist
+    deathLocation?: string;
+
+
 }
