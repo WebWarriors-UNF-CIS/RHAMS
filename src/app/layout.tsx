0533141695df85d27017255bb6d4d6b3dac52951
Harrison <hmichas@gmail.com>
2024-04-22 16:22:33 -0400

    Updated font settings in layout per shadcn-ui documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
+ 
+import { cn } from "@/lib/utils"
+ 
+const fontSans = FontSans
+({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
+export const metadata: Metadata = 
+{
   title: "The Artwork of Reuban Hale",
   description: "Artwork Management System for The Artwork of Reuban Hale, Inc.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout
+({children,}: Readonly<{children: React.ReactNode;}>) 
+{
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        ...
+      </body>
     </html>
-  );
+  )
 }
