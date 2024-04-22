da2dc0a773bb8fe9d701936df7696bb7618695f1
Harrison <hmichas@gmail.com>
2024-04-22 14:20:49 -0400

    Artwork Obj v1 - Basic Fields and commented adv fields

--- src/shared/artwork.ts
+++ src/shared/artwork.ts
@@ -1,4 +1,4 @@
-import { Entity, Fields } from 'remult'
+import { Entity, Fields, Relations } from 'remult'
 
 @Entity('artwork', {
   allowApiCrud: true,
@@ -6,9 +6,41 @@ import { Entity, Fields } from 'remult'
 export class Artwork {
     @Fields.integer()
     id = ''
+
     @Fields.createdAt()
     createdAt = new Date()
+
     @Fields.updatedAt()
     updatedAt = new Date()
   
+    //@Relations - > Artist
+
+    @Fields.string()
+    title = ''
+
+    //@Relations/Valuelist - > Mediums**?? **ask about this
+
+    //@Fields.json() - > Measurements
+
+    @Fields.dateOnly()
+    releaseDate = new Date()
+
+    //@Relations - > Collections
+
+    //@Fields.string()/.json()/Valuelist? - > Location (i.e Museum, Gallery, Living Room, Piano Room, etc) 
+    //   or 
+    //@Relations - > Location (i.e. New York, Paris, etc) **ask about this
+
+    //@Relations - > Exhibitions
+
+    @Fields.string({ allowNull: true })
+    notes = ''
+
+    @Fields.boolean()
+    inPortfolioBook = false
+
+    @Fields.string()
+    thumbnail = ''
+
+    //@Relations - > Editions
 }
