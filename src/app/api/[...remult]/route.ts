9fea9c60c74db3ead642c4b028e1e405869422cb
Harrison <hmichas@gmail.com>
2024-04-21 20:34:09 -0400

    connected remult to postgres server + added location entity

--- src/app/api/[...remult]/route.ts
+++ src/app/api/[...remult]/route.ts
@@ -4,13 +4,20 @@ import { Artwork } from "../../../shared/artwork"
 import { Buyer } from "../../../shared/buyer"
 import { Collection } from "../../../shared/collection"
 import { Exhibition } from "../../../shared/exhibition"
+import { Location } from "../../../shared/location"
 import { Media } from "../../../shared/media"
 import { SalesRec } from "../../../shared/salesrec"
 import { User } from "../../../shared/user"
+import { createPostgresDataProvider } from "remult/postgres"
+
+const DATABASE_URL = process.env["POSTGRES_URL"]
 
 const api = remultNextApp({
     entities: [Artist, Artwork, Buyer, Collection, 
-               Exhibition, Media, SalesRec, User],
+               Exhibition, Location, Media, SalesRec, User],
+    dataProvider: DATABASE_URL
+    ? createPostgresDataProvider({ connectionString: DATABASE_URL })
+    : undefined,
 })
 
 export const { POST, PUT, DELETE, GET } = api
