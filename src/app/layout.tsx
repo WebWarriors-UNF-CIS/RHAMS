e6d2641be750e2a537dc6de0bda0efb93ad3882f
Harrison <hmichas@gmail.com>
2024-04-22 17:53:33 -0400

    added favicon call in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export const metadata: Metadata =
 {
   title: "The Artwork of Reuban Hale",
   description: "Artwork Management System for The Artwork of Reuban Hale, Inc.",
+  icons: {
+    icon: "./favicon.ico",
+  },
 };
 
 export default function RootLayout
