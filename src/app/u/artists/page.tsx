f55693cb94c0b0357b90e2ecd24f53a20efb12db
amberredmond <n01456296@unf.edu>
2024-04-23 03:29:18 -0400

    added react-select to artists- added add artist button- changed things around

--- src/app/u/artists/page.tsx
+++ src/app/u/artists/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 import Image from "next/image";
 import React from 'react';
+import Creatable from 'react-select/creatable';
 
 export default function ArtistOverview() {
   const router = useRouter();
@@ -18,7 +19,7 @@ export default function ArtistOverview() {
     if (pathname)   
     {
       const parts = pathname.split('/');
-      setSlug(reformatTitle(parts[2]) + ' Placeholder Page');
+      setSlug(reformatTitle(parts[2]) + ' ');
     }
   }, [pathname]);
 
@@ -28,23 +29,19 @@ export default function ArtistOverview() {
       {slug || 'Loading...'}
       </h1>
       <div className="flex flex-row">
-        <div className="basis-1/4">
-          <Image src="https://via.placeholder.com/150" alt="placeholder" />
-        </div>
-        <div className="basis-1/4">
-          <Image src="https://via.placeholder.com/150" alt="placeholder" />
+        <div className="basis-1/2">
+          <h2 className="text-2xl font-bold">Search</h2>
         </div>
         <div className="basis-1/2">
-          <Image src="https://via.placeholder.com/300" alt="placeholder" />
+        <button  onClick={() => router.push('./artists/create')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-lg" type="button">
+            Add Artist
+        </button>
         </div>
         <div className="flex">
           {/* table */}
         </div>
       </div>
       <div className="flex flex-row justify-end gap-6 p-32">
-        <button  onClick={() => router.push('./artists/create')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-lg" type="button">
-            Next page
-        </button>
         <button  onClick={() => router.push('/')} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-lg" type="button">
             Back
         </button>
