fb352b3f7bd1bf8c732e6ede5ddb838db8cf6d75
Harrison <hmichas@gmail.com>
2024-04-22 21:09:55 -0400

    temporary webpages

--- src/app/u/artists/create/page.tsx
+++ src/app/u/artists/create/page.tsx
@@ -1,4 +1,30 @@
-import { Remult } from 'remult';
-import { Artist } from '../../../../shared/artist'; // import the Artist entity
-import { ValueList } from '../../../../shared/valuelist'; // import the Location entity
-import { Artwork } from '../../../../shared/artwork'; // import the Artwork entity
+"use client"
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+const Page = () => {
+  const router = useRouter();
+  const [slug, setSlug] = useState<string>('');
+
+  useEffect(() => 
+    {
+        if (router.isReady) 
+        {
+        const querySlug = router.query.slug as string;
+        setSlug(querySlug);
+        }
+    }, [router.isReady, router.query.slug]);
+
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold text-center my-10">
+        Title: {slug || 'Loading...'}
+      </h1>
+        <button  onClick={() => router.push('./')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="button">
+            Next page
+        </button>
+    </div>
+  );
+};
+
+export default Page;
