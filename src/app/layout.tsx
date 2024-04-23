cfd556c7060940fb31a4e8c18ba9f26962a598c1
Harrison <hmichas@gmail.com>
2024-04-22 20:53:16 -0400

    Added Vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
  
 import { cn } from "@/lib/utils"
@@ -28,7 +29,8 @@ export default function RootLayout
       </head>
       <body className={cn("min-h-screen bg-background font-sans antialiased",fontSans.variable)}>
         {children}
-        <SpeedInsights/>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
