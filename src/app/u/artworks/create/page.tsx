577d3d4ab6204b76a3ec58ebb848d4fc07af11ef
amberredmond <n01456296@unf.edu>
2024-04-25 02:29:16 -0400

    added placeholder code for artworks/create

--- src/app/u/artworks/create/page.tsx
+++ src/app/u/artworks/create/page.tsx
@@ -28,20 +28,45 @@ export default function CreateArtwork() {
       <h1 className="text-4xl font-bold text-center my-10">
       {slug || 'Loading...'}
       </h1>
-      <div className="flex flex-row">
-        <div className="basis-1/4">
-          <Image loader={imageLoader} src="150" alt="placeholder" width={150} height={150}/>
+      <div className="container mx-auto p-6 bg-white shadow-md">
+      <div className="flex justify-between">
+        <div className="w-1/2 p-4">
+          <div className="bg-gray-200 aspect-w-1 aspect-h-1 flex justify-center items-center">
+            <span className="text-gray-500 h-56">Image preview</span>
+          </div>
+          <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded">
+            Upload Image
+          </button>
         </div>
-        <div className="basis-1/4">
-          <Image loader={imageLoader} src="150" alt="placeholder" width={150} height={150}/>
-        </div>
-        <div className="basis-1/2">
-          <Image loader={imageLoader} src="300" alt="placeholder" width={300} height={300}/>
-        </div>
-        <div className="flex">
-          {/* table */}
+        <div className="w-1/2 p-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Artwork Title
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="title"
+              type="text"
+              placeholder="Title"
+            />
+          </div>
+          {/* ... other input fields ... */}
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Notes
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="notes"
+              placeholder="Enter notes about the artwork here..."
+            ></textarea>
+          </div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Save Record
+          </button>
         </div>
       </div>
+    </div>
       <div className="flex flex-row justify-end gap-6 p-32">
         <button  onClick={() => router.push('/u/collections')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-lg" type="button">
             Next page
