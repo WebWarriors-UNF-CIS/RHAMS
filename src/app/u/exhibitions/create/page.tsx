d83120e1e9fbbfe7b209317917c2669156fe8786
amberredmond <n01456296@unf.edu>
2024-04-24 23:32:47 -0400

    added users placeholder code and exhibitions/create placeholder

--- src/app/u/exhibitions/create/page.tsx
+++ src/app/u/exhibitions/create/page.tsx
@@ -28,20 +28,38 @@ export default function CreateExhibition() {
       <h1 className="text-4xl font-bold text-center my-10">
       {slug || 'Loading...'}
       </h1>
-      <div className="flex flex-row">
-        <div className="basis-1/4">
-          <Image loader={imageLoader} src="150" alt="placeholder" width={150} height={150}/>
+      <div className="container mx-auto p-6">
+      {/* Upper Form Section */}
+      <div className="bg-gray-200 p-4 mb-6">
+        <div className="mb-4">Details Placeholder</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+          <div className="bg-gray-300 p-4">Form Field Placeholder</div>
+          {/* Repeat for the number of fields */}
         </div>
-        <div className="basis-1/4">
-          <Image loader={imageLoader} src="150" alt="placeholder" width={150} height={150}/>
-        </div>
-        <div className="basis-1/2">
-          <Image loader={imageLoader} src="300" alt="placeholder" width={300} height={300}/>
-        </div>
-        <div className="flex">
-          {/* table */}
-        </div>
+        <div className="bg-gray-300 p-4">Notes Placeholder</div>
+        <div className="bg-green-200 p-2 mt-4 w-1/4 self-end">Save Record Placeholder</div>
       </div>
+
+      {/* Bottom Table Section */}
+      <div className="flex flex-col">
+      <div className="flex justify-between items-center p-4 bg-gray-100">
+      {/* Export to Excel Placeholder */}
+      <div className="bg-green-200 p-2 text-sm">Export to Excel Placeholder</div>
+
+      {/* Search Placeholder */}
+      <div className="flex items-center bg-white border rounded overflow-hidden">
+        <input
+          className="px-4 py-2"
+          placeholder="Add Artwork by Title or File #"
+        />
+        <button className="bg-green-500 p-2">
+          {/* Placeholder for the search icon */}
+        </button>
+      </div>
+    </div>
+        <div className="bg-gray-300 h-72  p-4">Table Placeholder</div>
+      </div>
+    </div>
       <div className="flex flex-row justify-end gap-6 p-32">
         <button  onClick={() => router.push('/u/media')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-lg" type="button">
             Next page
