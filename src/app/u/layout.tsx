123c558c9344d11657346e2205b3850c01aa3489
Harrison <hmichas@gmail.com>
2024-04-26 23:32:02 -0400

    navbar layout temp comments

--- src/app/u/layout.tsx
+++ src/app/u/layout.tsx
@@ -1,6 +1,15 @@
 "use client";
 import Navbar from '../../components/navbar';
+ /* 
+    Set up title metadata for the /u/ pages
+    import type { Metadata } from "next";
 
+
+    export const metadata: Metadata = 
+    {
+    title: "[...slug]", 
+    };
+*/
 export default function NavbarLayout
 ({children,}: {children: React.ReactNode;}) 
 {
