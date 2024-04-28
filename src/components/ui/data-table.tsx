f08bd74d972b69d618befb4baadd95513ee1cce4
amberredmond <n01456296@unf.edu>
2024-04-28 08:07:22 -0400

    added pagination code to data-table

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -4,6 +4,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 
@@ -29,6 +30,7 @@ export function DataTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   })
 
   return (
