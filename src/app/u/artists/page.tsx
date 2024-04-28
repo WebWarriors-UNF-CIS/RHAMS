a0810e4bc152786c212248a3543542607691cdae
amberredmond <n01456296@unf.edu>
2024-04-28 04:28:50 -0400

    Revert "updated artists page.tsx with table"
    This reverts commit f6d4ed1050ab2907114931bddd6c1043e5d429c3.

--- src/app/u/artists/page.tsx
+++ src/app/u/artists/page.tsx
@@ -8,9 +8,14 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
 import { Button } from '../../../components/ui/button';
 import React from 'react';
 import Image from "next/image";
+import React from 'react';
+import Creatable from 'react-select/creatable';
 import Link from 'next/link';
 import { ArtistData, columns } from "../../../components/ui/columns/artist-overview"
 import { DataTable } from "@/components/ui/data-table"
+import { remult, EntityFilter} from 'remult';
+import { Artist } from '../../../shared/artist';
+import { Button } from '../../../components/ui/button';
 
 
 const repo = remult.repo<Artist>(Artist);
@@ -18,7 +23,6 @@ const repo = remult.repo<Artist>(Artist);
 export default function ArtistsOverview() 
 {
   const router = useRouter();
-  const [artist, setArtists] = useState<Artist[]>([]);
   const [slug, setSlug] = useState<string>('');
   const pathname = usePathname();
   const imageLoader = ({src}: {src: string}) => {return `https://via.placeholder.com/${src}`}
@@ -28,18 +32,23 @@ export default function ArtistsOverview()
   function reformatTitle(input: string) 
   {return input.charAt(0).toUpperCase() + input.slice(1);}
   useEffect(() => 
-    {if (pathname) { const parts = pathname.split('/'); setSlug(reformatTitle(parts[2]) + ' ')}}, [pathname]);
-  useEffect(() => 
+  {
+    if (pathname)   
+    {
+      const parts = pathname.split('/');
+      setSlug(reformatTitle(parts[2]) + ' ');
+    }
+  }, [pathname]);
+  useEffect(() =>
     {repo.find({}).then(artists => setArtists(artists))} , [remult]);
-  let entries = artist.map(artist => 
-    [artist.id, artist, artist.thumbnail, 
-      artist.firstName, artist.lastName, 
-      artist.bio, artist.birthDate, artist.deathDate, 
-      artist.birthLocation, artist.deathLocation, 
+  let entries = artist.map(artist =>
+    [artist.id, artist, artist.thumbnail,
+      artist.firstName, artist.lastName,
+      artist.bio, artist.birthDate, artist.deathDate,
+      artist.birthLocation, artist.deathLocation,
       artist.notes, artist.artworks, artist.exhibitions]);
     const data = Object.fromEntries(entries);
-    
-  
+ 
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-4xl font-bold text-center my-10">
@@ -53,13 +62,7 @@ export default function ArtistsOverview()
         </div>
         <div className="bg-purple-200 p-4 text-center">Search Placeholder</div>
       </div>
-      <div className="bg-gray-300 h-96 p-8 text-center w-full">
-      
-      <div className="container mx-auto py-10">
       <DataTable columns={columns} data={data} />
-    </div>
-        
-      </div>
     </div>
       <div className="flex flex-row justify-end gap-6 p-32">
         <button  onClick={() => router.push('/')} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-lg" type="button">
