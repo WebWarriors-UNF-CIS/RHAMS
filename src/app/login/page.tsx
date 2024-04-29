e907ee9be8d235f5cbeba306e14c01efc7baf99b
amberredmond <n01456296@unf.edu>
2024-04-29 16:20:08 -0400

    added login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,15 +12,43 @@ import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
 import Image from "next/image";
 import Logo from "../../public/images/logo-w.png";
 import React from 'react';
+import * as z from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form';
+import e from 'express';
+import { Form, FormField, FormItem, FormMessage, FormLabel, FormControl, FormDescription } from '@/components/ui/form';
+import { CalendarIcon } from "lucide-react"
+import { format } from "date-fns"
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {Popover, PopoverContent, PopoverTrigger,} from "@/components/ui/popover"
+import { Textarea } from "@/components/ui/textarea"
+import { toast } from "@/components/ui/use-toast"
 
 
 const userRepo = remult.repo(User);
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  passwordConfirm: z.string().min(8),
+  
+})
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
-
+  const handleSubmit = () => {}
+  const [date, setDate] = React.useState<Date>()
+  const form = useForm<z.infer <typeof formSchema>>({
+    resolver: zodResolver(formSchema)});
+    defaultValues: {
+      email: ''
+      password: ''
+      passwordConfirm: ''
+    }
+    
   const login = async (e: React.FormEvent) => 
     {
     e.preventDefault();
@@ -61,45 +89,71 @@ export default function Login() {
           <div className="m-12 mb-32">
             <Image src={Logo} alt="RHA-Logo" width={250} height={250} />
           </div>
-          <form onSubmit={login} className="w-full px-12">
-            <div className="mb-12">
-              <label htmlFor="email" className="block text-c text-sm font-bold mb-2">Username</label>
-              <Input
-                type="email"
-                id="email"
-                value={email}
-                placeholder='me@domain.com'
-                onChange={(e) => setEmail(e.target.value)}
-                className="input bg-input border border-border rounded-md py-2 px-3 leading-tight focus:outline-none focus:ring"
-              />
-            </div>
-            <div className="mb-12">
-              <label htmlFor="password" className="block text-c text-sm font-bold mb-2">Password</label>
-              <Input
-                type="password"
-                id="password"
-                placeholder='********'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="input bg-input border border-border rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:ring"
-              />
-            </div>
-            <div className="flex items-center justify-between px-2 mb-12">
-              <div className="flex flex-none items-center space-x-2">
-                  <Checkbox id="rememberme" />
-                  <label
-                    htmlFor="rememberme"
-                    className="text-sm text-c"> Remember Me 
-                  </label>
-              </div>
-              <button onClick={() => router.push('/u/artists')} className="text-sm text-c hover:text-a">Forgot Password?</button>
-            </div>
-            <div className="flex justify-center items-center">
-              <Button type="submit" className="bg-a py-2 px-16 rounded text-c">
-                Log In
-              </Button>
-            </div>
-          </form>
+          <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="max-w-md w-full flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-white">Email address</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Email address"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          
+          
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-white">Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-white">Password confirm</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Password confirm"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
         </div>
       </div>
     </div>
