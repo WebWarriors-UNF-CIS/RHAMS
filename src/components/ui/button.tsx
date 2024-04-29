ec7bdd54b6957fe0f928a25a29579933ed7617fa
Harrison <hmichas@gmail.com>
2024-04-29 01:48:53 -0400

    UI Improvments - Data Table components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,34 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-a text-c shadow hover:bg-a/90",
+          "bg-a text-c shadow hover:bg-a/90 rounded-md",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 rounded-md",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground rounded-md",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 rounded-md",
+        defaultCirclular:
+          "bg-a text-c shadow hover:bg-a/90 rounded-full",
+        destructiveCircular:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 rounded-full",
+        outlineCircular:
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground rounded-full",
+        secondaryCircular:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 rounded-full",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-9 px-2 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        icon: "h-6 w-6",
       },
     },
     defaultVariants: {
--- src/components/ui/columns/artist-overview.tsx
+++ src/components/ui/columns/artist-overview.tsx
@@ -1,19 +1,48 @@
 import { ColumnDef } from "@tanstack/react-table"
+import { Badge } from "@/components/ui/badge"
+import { Checkbox } from "@/components/ui/checkbox"
  
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type ArtistData = {
-  firstName: string
+export type ArtistData = 
+{
+  thumbnail: string
   lastName: string
+  firstName: string
 }
  
-export const columns: ColumnDef<ArtistData>[] = [
+export const columns: ColumnDef<ArtistData>[] = 
+[
   {
-    accessorKey: "firstName",
-    header: "First Name",
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    accessorKey: "thumbnail",
+    header: " ",
   },
   {
     accessorKey: "lastName",
     header: "Last Name",
+  },
+  {
+    accessorKey: "firstName",
+    header: "First Name",
   },
 ]
